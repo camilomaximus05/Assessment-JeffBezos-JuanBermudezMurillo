9e3314c148c4946a86cc775418de7ad32c1df34a
camilomaximus005 <jbermudezmu@unal.edu.co>
2024-12-09 00:34:59 -0500

    feat: 30-create user and role controllers

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,12 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
+    use HasRoles;
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
@@ -27,6 +29,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastName',
         'email',
         'password',
     ];

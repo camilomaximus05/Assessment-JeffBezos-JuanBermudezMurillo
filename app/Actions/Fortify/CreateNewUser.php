e01661181dc8828a8a049dc23ffe509237a60f26
camilomaximus005 <jbermudezmu@unal.edu.co>
2024-12-09 00:30:53 -0500

    feat: 29-Install Spatie Laravel Permission package

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -20,14 +20,16 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:25'],
+            'lastName'=>['required', 'string', 'max:25'],
+            'email' => ['required', 'string', 'email', 'max:30', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
         return User::create([
             'name' => $input['name'],
+            'lastName' => $input['lastName'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
